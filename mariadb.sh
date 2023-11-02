cf2ca8f50c14aef89340f2218d46204f2c0bc559
Nassima Ayadi <gsmicrodev@gmail.com>
2023-11-02 10:08:07 +0100

    corrections

--- mariadb.sh
+++ mariadb.sh
@@ -10,14 +10,14 @@ if ! [ -x "$(command -v mariadb)" ]; then
 fi
 
 # Créer la base de données avec le nom du projet
-sudo mariadb -e "CREATE DATABASE $project_name;"
+sudo mariadb -u root -p -e "CREATE DATABASE $project_name;"
 
 # Créer un utilisateur avec le nom du projet et un mot de passe aléatoire
 password=$(openssl rand -base64 12)
-sudo mariadb -e "CREATE USER '$project_name'@'localhost' IDENTIFIED BY '$password';"
+sudo mariadb -u root -p -e "CREATE USER '$project_name'@'localhost' IDENTIFIED BY '$password';"
 
 # Accorder tous les droits à l'utilisateur sur la base de données
-sudo mariadb -e "GRANT ALL PRIVILEGES ON $project_name.* TO '$project_name'@'localhost';"
+sudo mariadb -u root -p -e "GRANT ALL PRIVILEGES ON $project_name.* TO '$project_name'@'localhost';"
 
 # Afficher les informations récapitulatives
 echo "Base de données $project_name créée avec succès."
